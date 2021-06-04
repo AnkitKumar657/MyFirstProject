75bc774e8718ddc8ff3c3b190a0e9ed849dece38
AnkitKumar657 <ankit.ce.kumar@accenture.com>
2021-06-04 07:22:47 +0530

    created customer and user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
